04f4a6d1c090e4b6c1ddf73ee69fdb6c054776c2
Meng To <shadownessguy@gmail.com>
2023-01-16 10:09:54 -0500

    Remove unnecessary siteUrl

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,6 @@
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
     `gatsby-plugin-image`,
